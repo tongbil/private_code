3df3352ca7512af5af4621a3ff523d48f6be7a5f
tongbil <45443093+tongbil@users.noreply.github.com>
2019-08-16 23:14:04 +0800

    上传
    第一次上传文件

---
+++ Demos.java
@@ -0,0 +1,31 @@
+package com.qixin.common;
+
+
+
+public class Demos implements Runnable{
+	private String num;
+	public Demos(int num){
+		this.num=num+"";
+	}
+		public static void main(String[] args) {
+			int[] nums={1,244,22,11,555};
+			for (int i = 0; i < nums.length; i++) {
+				new Thread(new Demos(nums[i])).start();
+			}
+		}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		 try {
+			Thread.sleep(Integer.parseInt(num));
+			System.out.println(num);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+}
