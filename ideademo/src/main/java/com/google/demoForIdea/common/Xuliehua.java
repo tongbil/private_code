149e4f873b8f8680290bfaa884e1dcfda00da4f1
tongbil <812568779@qq.com>
2020-02-19 09:16:11 +0800

    '文件上传下载'

--- ideademo/src/main/java/com/google/demoForIdea/common/Xuliehua.java
+++ ideademo/src/main/java/com/google/demoForIdea/common/Xuliehua.java
@@ -5,10 +5,22 @@ import java.io.*;
 
 public class Xuliehua {
     	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		   /* User user = new User();
+		    user.setUsername("汤彪");
+		    byte[] bytes = toBytes(user);
+		    System.out.println(bytes);
+		    User user2 = (User) toObj(bytes);
+		    System.out.println(user2.getUsername()+"反序列化");
 
+		    toFile(user, "C:/Users/tangcomes/Desktop/console.txt");
+		    User user4 = (User) fromFile("C:/Users/tangcomes/Desktop/console.txt");
+		    System.out.println(user4.getUsername()+"我读");*/
+/*
     			        
     			        String path ="C:/Users/tangcomes/Desktop/27console.txt";
+		    String pathWrite ="C:/Users/tangcomes/Desktop/console.txt";
 		    System.out.println( read(path));
+		   writeFile(pathWrite,"汤彪");*/
 }
 
 
@@ -43,15 +55,20 @@ public class Xuliehua {
 			File f = new File(fileName);
 		 if(f.isFile()&&f.exists())
 		 {
+		 	//做的操作是将字节流转换成字符流
 		 	InputStreamReader read = null;
+		 	//new FileInputStream(f)是创建输入流,此时读取的是文件的字节流，in.read()读取的是字节
 		 read = new InputStreamReader(new FileInputStream(f),"gbk");
+			// 创建一个reader的字符留缓冲区，将字符装载入缓冲区中
 			 BufferedReader reader=new BufferedReader(read);
 			 String line;
+			 //用缓冲区的readLine(行的读取，返回值为String)读取文件
 		while ((line = reader.readLine()) != null)
 		 {
 		fileContent += line;
 		 }
-		read.close();
+
+			 read.close();
 		 }
 	} catch (Exception e)
 	{
